2f7a44c7f3b5c32b8fd88080a094730719403379
UtkarshPingale <utkarshpingale11@gamil.com>
2024-10-26 22:56:31 +0530

    remove all files. restart

--- index-script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector(".menu-btn").addEventListener("click", function() {
-        console.log("Menu button clicked!");
-        document.querySelector(".sidebar").classList.toggle("active");
-    });
-});
